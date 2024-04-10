8c764652bfd3a2dd19eff7db8141426077cd9693
kausikrstnj <kausikrstnj@gmail.com>
2024-04-10 21:21:41 +0530

    mongodb+srv:

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_SECRET_KEY",
-  mongoUri: process.env.DB_URL || "mongodb://localhost:27017/merntemplate",
+  mongoUri: process.env.DB_URL || "mongodb+srv://guvi:guvi@kausik.96vzqei.mongodb.net/ZenClass",
 };
 
 module.exports = config;
